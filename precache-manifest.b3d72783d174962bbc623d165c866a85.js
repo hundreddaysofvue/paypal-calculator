e95d161f2cad85cec0ca558188397e4f06fbc7e7
Jhony Grillet <jgrillet@mppp.gob.ve>
2020-03-30 11:54:24 -0400

    deploy

---
+++ precache-manifest.b3d72783d174962bbc623d165c866a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd74e340ecd45ce4acba",
+    "url": "/paypal-calculator/css/app.29144f10.css"
+  },
+  {
+    "revision": "bd74e340ecd45ce4acba",
+    "url": "/paypal-calculator/js/app.d9a95781.js"
+  },
+  {
+    "revision": "72369cb240c9b32b6fde",
+    "url": "/paypal-calculator/js/chunk-vendors.5931fbef.js"
+  },
+  {
+    "revision": "1034b748f733b38cebc533ab0bd8ad38",
+    "url": "/paypal-calculator/img/send-receive-icon.1034b748.svg"
+  },
+  {
+    "revision": "ec5d3991dda679d97d1d6e8246e55f12",
+    "url": "/paypal-calculator/img/paypal-calculator-logo.ec5d3991.svg"
+  },
+  {
+    "revision": "4593e64b511bde8489804bf70095ce48",
+    "url": "/paypal-calculator/index.html"
+  }
+];
